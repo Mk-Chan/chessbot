010439fdd85c8549777289a653da2fc4e2b8a211
Manik Charan <mkchan2951@gmail.com>
2019-10-28 20:33:26 +0530

    Replace elo calculation with Twipply's code

--- commands/match.py
+++ commands/match.py
@@ -4,6 +4,78 @@ from commands.base import BaseCommand
 from utils import filtered_split
 
 
+class Elo():
+    def __init__(self, wins, losses, draws):
+        self.wins = wins
+        self.losses = losses
+        self.draws = draws
+
+    def erfInv(self, x):
+        assert x < 1.0
+        assert x > -1.0
+        a = 8.0 * (math.pi - 3.0) / (3.0 * math.pi * (4.0 - math.pi))
+        y = math.log(1.0 - x * x)
+        z = 2.0 / (math.pi * a) + y / 2.0
+        ret = math.sqrt(math.sqrt(z * z - y / a) - z)
+        return ret if x >= 0.0 else -ret
+
+    def phiInv(self, p):
+        return math.sqrt(2.0) * self.erfInv(2.0 * p - 1.0)
+
+    def diff(self, p):
+        if p >= 1.0:
+            return math.inf
+        elif p <= 0.0:
+            return -math.inf
+        return -400.0 * math.log(1.0 / p - 1.0, 10)
+
+    def elo(self):
+        total = self.wins + self.losses + self.draws
+
+        if total == 0:
+            return math.nan
+
+        m_mu = (self.wins + self.draws / 2.0) / total
+
+        x = self.diff(m_mu)
+        return 0.0 if x == -0.0 else x
+
+    def err(self):
+        total = self.wins + self.losses + self.draws
+
+        if total == 0:
+            return math.nan
+
+        m_mu = (self.wins + self.draws / 2.0) / total
+
+        w = self.wins / total
+        l = self.losses / total
+        d = self.draws / total
+
+        devW = w * math.pow(1.0 - m_mu, 2.0)
+        devL = l * math.pow(0.0 - m_mu, 2.0)
+        devD = d * math.pow(0.5 - m_mu, 2.0)
+
+        m_stdev = math.sqrt(devW + devL + devD) / math.sqrt(total)
+
+        muMin = m_mu + self.phiInv(0.025) * m_stdev
+        muMax = m_mu + self.phiInv(0.975) * m_stdev
+        return (self.diff(muMax) - self.diff(muMin)) / 2.0
+
+    def los(self):
+        if self.wins + self.losses == 0:
+            return math.nan
+        return 100 * (0.5 + 0.5 * math.erf((self.wins - self.losses)/math.sqrt(2.0 * (self.wins + self.losses))))
+
+    def drawRatio(self):
+        total = self.wins + self.losses + self.draws
+
+        if total == 0:
+            return math.nan
+
+        return self.draws / total
+
+
 class Match(BaseCommand):
     name = "match"
 
@@ -22,39 +94,10 @@ class Match(BaseCommand):
         except ValueError:
             return " All values must be integers."
 
-        if wins < 2 or losses < 2 or draws < 2:
-            return "Wins, losses and draws must all be greater than 1."
+        if wins < 0 or losses < 0 or draws < 0:
+            return " All values must be >= 0"
 
-        n = float(wins + losses + draws)
-        wins /= n
-        losses /= n
-        draws /= n
-        m_mu = wins + draws / 2.0
+        elo = Elo(wins, losses, draws)
 
-        dev_w = wins * pow(1.0 - m_mu, 2.0)
-        dev_l = losses * pow(0.0 - m_mu, 2.0)
-        dev_d = draws * pow(0.5 - m_mu, 2.0)
-        m_std_dev = math.sqrt(dev_w + dev_l + dev_d) / math.sqrt(n)
-
-        def diff(p):
-            return -400.0 * math.log10(1.0 / p - 1.0)
-
-        elo_diff = diff(m_mu)
-
-        def erf_inv(x):
-            pi = math.pi
-            a = 8.0 * (pi - 3.0) / (3.0 * pi * (4.0 - pi))
-            y = math.log(1.0 - x * x)
-            z = 2.0 / (pi * a) + y / 2.0
-            ret = math.sqrt(math.sqrt(z * z - y / a) - z)
-            return ret if x >= 0.0 else -ret
-
-        def phi_inv(p):
-            return math.sqrt(2.0) * erf_inv(2.0 * p - 1.0)
-
-        mu_min = m_mu + phi_inv(0.025) * m_std_dev
-        mu_max = m_mu + phi_inv(0.975) * m_std_dev
-        err = (diff(mu_max) - diff(mu_min)) / 2.0
-
-        return " ".join(["Elo diff:", str(round(elo_diff, 2)),
-                         "±", str(round(err, 2))])
+        return " ".join(["Elo diff:", str(round(elo.elo(), 2)),
+                         "±", str(round(elo.err(), 2))])
